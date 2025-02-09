46f660415366d4c0f3c51ba006158251978e558d
rdel-olm <rdel-olm@student.42malaga.com>
2025-02-09 17:53:24 +0100

    In progress ex02

---
+++ Module_00/ex02/src/Account.cpp
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdel-olm <rdel-olm@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 21:11:56 by rdel-olm          #+#    #+#             */
+/*   Updated: 2025/02/09 17:52:40 by rdel-olm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <iomanip>
+
+#include "../includes/Account.hpp"
+#include "../includes/macros.hpp"
+#include "../includes/colors.hpp"
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+Account::Account(int initial_deposit)
+{
+
+
+}
+
+Account::~Account(void)
+{
+
+
+
+}
+
+void	Account::makeDeposit(int deposit)
+{
+
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+
+}
+
+int		Account::checkAmount(void) const
+{
+
+
+}
+
+void	Account::displayStatus(void) const
+{
+
+
+}
+
+void	Account::_displayTimestamp(void)
+{
+
+
+}
+
+int	Account::getNbAccounts(void)
+{
+
+
+}
+
+int	Account::getTotalAmount(void)
+{
+
+
+}
+
+int	Account::getNbDeposits(void)
+{
+
+
+}
+
+int	Account::getNbWithdrawals(void)
+{
+
+
+}
+
+void	Account::displayAccountsInfos(void)
+{
+
+
+}
