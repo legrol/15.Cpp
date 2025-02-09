056a16ffe98fea8ad59b83d011db3307a16b6ead
rdel-olm <rdel-olm@student.42malaga.com>
2025-02-09 21:23:43 +0100

    Complet ex02

--- Module_00/ex02/src/Account.cpp
+++ Module_00/ex02/src/Account.cpp
@@ -6,16 +6,15 @@
 /*   By: rdel-olm <rdel-olm@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 21:11:56 by rdel-olm          #+#    #+#             */
-/*   Updated: 2025/02/09 17:52:40 by rdel-olm         ###   ########.fr       */
+/*   Updated: 2025/02/09 21:17:24 by rdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <iomanip>
+#include <ctime>
 
 #include "../includes/Account.hpp"
-#include "../includes/macros.hpp"
-#include "../includes/colors.hpp"
 
 /**
  * 
@@ -25,73 +24,112 @@
  * 
  */
 
+int	Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
 Account::Account(int initial_deposit)
 {
-
-
+	this->_accountIndex = _nbAccounts++;
+	this->_amount = initial_deposit;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	this->_totalAmount += initial_deposit;
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" \
+	<< this->_amount << ";created" << std::endl;
 }
 
 Account::~Account(void)
 {
-
-
-
-}
-
-void	Account::makeDeposit(int deposit)
-{
-
-}
-
-bool	Account::makeWithdrawal(int withdrawal)
-{
-
-}
-
-int		Account::checkAmount(void) const
-{
-
-
-}
-
-void	Account::displayStatus(void) const
-{
-
-
-}
-
-void	Account::_displayTimestamp(void)
-{
-
-
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" \
+	<< this->_amount << ";closed" << std::endl;
 }
 
 int	Account::getNbAccounts(void)
 {
-
-
+	return (Account::_nbAccounts);
 }
 
 int	Account::getTotalAmount(void)
 {
-
-
+	return (Account::_totalAmount);
 }
 
 int	Account::getNbDeposits(void)
 {
-
-
+	return (Account::_totalNbDeposits);
 }
 
 int	Account::getNbWithdrawals(void)
 {
-
-
+	return (Account::_totalNbWithdrawals);
 }
 
 void	Account::displayAccountsInfos(void)
 {
+	Account::_displayTimestamp();
+	std::cout << "accounts:" << Account::getNbAccounts() << \
+	";total:" << Account::getTotalAmount() << \
+	";deposits:" << Account::getNbDeposits() << \
+	";withdrawals:" << Account::getNbWithdrawals() << std::endl;
+}
 
+void	Account::makeDeposit(int deposit)
+{
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";p_amount:" \
+	<< this->_amount;
+	this->_amount += deposit;
+	this->_nbDeposits += 1;
+	_totalAmount += deposit;
+	_totalNbDeposits += 1;
+	std::cout << ";deposit:" << deposit << ";amount:" \
+	<< this->_amount << ";nb_deposits:" << this->_nbDeposits << std::endl;
+}
 
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex <<  ";p_amount:" << this->_amount;
+	this->_amount -= withdrawal;
+	if (checkAmount())
+	{
+		this->_amount += withdrawal;
+		std::cout << ";withdrawal:refused" << std::endl;
+		return 0;
+	}
+	_totalAmount -= withdrawal;
+	this->_nbWithdrawals += 1;
+	std::cout << ";withdrawal:" << withdrawal << ";amount:" << this->_amount \
+	<< ";nb_withdrawals:" << this->_nbWithdrawals << std::endl;
+	return 1;
+}
+
+int		Account::checkAmount(void) const
+{
+	if (this->_amount < 0)
+		return 1;
+	return 0;
+}
+
+void	Account::displayStatus(void) const
+{
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << \
+	this->_amount << ";deposits:" << this->_nbDeposits << ";withdrawals:" << \
+	this->_nbWithdrawals << std::endl;
+}
+
+void	Account::_displayTimestamp(void)
+{
+	time_t at_once;
+
+	at_once = time(NULL);
+	struct tm *tm_info = localtime(&at_once);
+	char buffer[20];
+	strftime(buffer, sizeof(buffer), "[%Y%m%d_%H%M%S] ", tm_info);
+	std::cout << buffer;
 }
