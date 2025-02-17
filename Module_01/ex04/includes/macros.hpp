4dc2a869375cbb8028faaf03a70742f80e2d5fea
rdel-olm <rdel-olm@student.42malaga.com>
2025-02-17 22:30:07 +0100

    Update complet Mod1ex04

--- Module_01/ex04/includes/macros.hpp
+++ Module_01/ex04/includes/macros.hpp
@@ -6,15 +6,19 @@
 /*   By: rdel-olm <rdel-olm@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 16:44:38 by rdel-olm          #+#    #+#             */
-/*   Updated: 2025/02/16 22:54:49 by rdel-olm         ###   ########.fr       */
+/*   Updated: 2025/02/17 21:52:47 by rdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MACROS_HPP
 # define	MACROS_HPP
 
-
-# define OPEN_ERR		"Error: could not open "
+# define ARG_ERR		"Startup parameters: (File to modify) (Word to search \
+for) (Replacement word)"
+# define ARG2_ERR		"Error: arg(Word to search for) is empty."
+# define OPEN_ERR		"Error: could not open file "
+# define VERIF_ERR		": Check the existence of the file or directory and \
+its permissions."
 
 
 void	print_banner();
--- Module_01/ex04/src/main.cpp
+++ Module_01/ex04/src/main.cpp
@@ -6,13 +6,14 @@
 /*   By: rdel-olm <rdel-olm@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 16:44:27 by rdel-olm          #+#    #+#             */
-/*   Updated: 2025/02/16 22:56:30 by rdel-olm         ###   ########.fr       */
+/*   Updated: 2025/02/17 22:29:24 by rdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <iostream>					// std::cout, std::endl, ...
 # include <string>						// std::string
 # include <cstdlib>						// use to EXIT_FAILURE, EXIT_SUCCESS...
+# include <fstream>						// std::ofstream, std::ifstream
 
 #include "../includes/colors.hpp"
 #include "../includes/macros.hpp"
@@ -28,25 +29,63 @@
 
 int	my_replace(char **argv, std::string chain)
 {
+	std::string		ocurrenceFirst = argv[2];
+	std::string		ocurrenceSecond = argv[3];
+	std::string		nameNewFile;
+	std::ofstream	fileFinal;
+	size_t			place;
 
+	nameNewFile = (std::string (argv[1])) + ".replace";
+	fileFinal.open(nameNewFile.c_str());
+	if (fileFinal.fail())
+		return (EXIT_FAILURE);
+	
+	for(size_t i = 0; i < chain.length(); i++)
+	{
+		place = chain.find(ocurrenceFirst, i);
+		if (place != std::string::npos && place == i)
+		{
+			fileFinal << ocurrenceSecond;
+			i += ocurrenceFirst.length() - 1;
+		}
+		else
+			fileFinal << chain[i];
+	}
 
-
-
-
+	fileFinal.close();
+	return (EXIT_SUCCESS);
 }
 
 int	main(int argc, char **argv)
 {
+	std::string			chain;
+	std::ifstream		fileInitial;
+	char				c;
+	
 	print_banner();
 	
-
-
 	if (argc != 4)
 	{
-		std::cout << OPEN_ERR << argv[1] ;
+		std::cout << BDRED ARG_ERR RESET << std::endl;
+		return (EXIT_FAILURE);		
+	}
+	if (argv[2][0] == '\0')
+	{
+		std::cout << BDRED ARG2_ERR RESET << std::endl;
 		return (EXIT_FAILURE);
-
-		
 	}
 
+	fileInitial.open(argv[1]);
+	if (!fileInitial.is_open())
+	{
+        std::cerr << BDRED OPEN_ERR RESET CYAN << argv[1] << RESET YELLOW \
+		VERIF_ERR RESET << std::endl;
+        return (EXIT_FAILURE);
+    }
+
+	while  (fileInitial >> std::noskipws >> c)
+		chain += c;
+
+	fileInitial.close();
+	return (my_replace(argv, chain));
 }
