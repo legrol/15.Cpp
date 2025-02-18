9255460f88db548847e0e55cb63c0a9ba6844862
rdel-olm <rdel-olm@student.42malaga.com>
2025-02-18 19:00:51 +0100

    Update complet Mod1ex05

--- Module_01/ex05/src/main.cpp
+++ Module_01/ex05/src/main.cpp
@@ -3,19 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rdel-olm <rdel-olm@student.42malaga.com>   #+#  +:+       +#+        */
+/*   By: rdel-olm <rdel-olm@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-02-18 08:48:09 by rdel-olm          #+#    #+#             */
-/*   Updated: 2025-02-18 08:48:09 by rdel-olm         ###   ########.fr       */
+/*   Created: 2025/02/18 08:48:09 by rdel-olm          #+#    #+#             */
+/*   Updated: 2025/02/18 18:59:48 by rdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Harl.hpp"
 
 /**
+ * The function `main` serves as the entry point of the program.
+ * It initializes an instance of the `Harl` class and sequentially 
+ * invokes the `complain` method for different complaint levels.
  * 
+ * @return int   Returns EXIT_SUCCESS upon successful execution.
  * 
- * 
+ * Process:
+ * - Instantiates an object of the `Harl` class named `reply`.
+ * - Calls the `print_banner` function to display an introductory message.
+ * - Invokes the `complain` method with different complaint levels:
+ *   - "DEBUG"   → Calls `Harl::debug`
+ *   - "INFO"    → Calls `Harl::info`
+ *   - "WARNING" → Calls `Harl::warning`
+ *   - "ERROR"   → Calls `Harl::error
  * 
  */
 
--- Module_01/ex05/utils/Harl.cpp
+++ Module_01/ex05/utils/Harl.cpp
@@ -3,23 +3,70 @@
 /*                                                        :::      ::::::::   */
 /*   Harl.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rdel-olm <rdel-olm@student.42malaga.com>   #+#  +:+       +#+        */
+/*   By: rdel-olm <rdel-olm@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-02-18 08:54:20 by rdel-olm          #+#    #+#             */
-/*   Updated: 2025-02-18 08:54:20 by rdel-olm         ###   ########.fr       */
+/*   Created: 2025/02/18 08:54:20 by rdel-olm          #+#    #+#             */
+/*   Updated: 2025/02/18 18:58:54 by rdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Harl.hpp"
 
 /**
+ * The function `Harl::Harl` is the default constructor for the `Harl` class.
+ * It initializes an instance of `Harl` but does not perform any specific 
+ * operations upon instantiation.
  * 
  * 
+ * The function `Harl::~Harl` is the destructor for the `Harl` class.
+ * It ensures that any allocated resources associated with the `Harl` 
+ * instance are cleaned up when the object goes out of scope.
  * 
  * 
+ * The function `Harl::complain` processes a given complaint level and 
+ * calls the corresponding member function (debug, info, warning, or error).
+ * 
+ * @param std::string level   A string representing the complaint level:
+ *                            - "DEBUG"
+ *                            - "INFO"
+ *                            - "WARNING"
+ *                            - "ERROR"
+ * 
+ * @return void               No return value.
+ * 
+ * Process:
+ * - Defines an array of function pointers, each pointing to a specific 
+ *   member function of `Harl`.
+ * - Defines an array of strings corresponding to the complaint levels.
+ * - Iterates through the `choice` array, comparing the input level.
+ * - If a match is found, the corresponding function is called.
  * 
  * 
+ * The function `Harl::debug` prints a debug message to the console.
+ * It represents the least severe complaint level, used for internal 
+ * debugging information.
  * 
+ * @return void   No return value.
+ * 
+ * 
+ * The function `Harl::info` prints an informational message to the console.
+ * This level provides additional details about the program's execution.
+ * 
+ * @return void   No return value.
+ * 
+ * 
+ * The function `Harl::warning` prints a warning message to the console.
+ * This level is used to indicate potential issues or situations that 
+ * might require attention.
+ * 
+ * @return void   No return value.
+ * 
+ * 
+ * The function `Harl::error` prints an error message to the console.
+ * This is the most severe level of complaint, indicating that something 
+ * has gone wrong.
+ * 
+ * @return void   No return value.
  * 
  */
 
@@ -36,16 +83,16 @@ Harl::~Harl()
 void Harl::complain(std::string level)
 {
 	void (Harl::*ptr_level[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
-	std::string	options[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	std::string	choice[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 
 	for (int i = 0; i < 4; i++)
 	{
-		if (options[i] == level)
+		if (choice[i] == level)
 			(this->*ptr_level[i])();
 	}
 }
 
-void	Harl::debug(void)
+void	Harl::debug(void) 
 {
 	std::cout << ORANGE DBG RESET << std::endl;
 	std::cout << CYAN DBG1 RESET<< std::endl;
