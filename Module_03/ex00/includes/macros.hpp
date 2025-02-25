8001a1c6191885d5f2cf9ffd7a28bd21e0cd3beb
rdel-olm <rdel-olm@student.42malaga.com>
2025-02-25 12:57:04 +0100

    Update complet Mod03ex00

--- Module_03/ex00/includes/macros.hpp
+++ Module_03/ex00/includes/macros.hpp
@@ -13,12 +13,24 @@
 #ifndef MACROS_HPP
 # define	MACROS_HPP
 
+# define MAX_HP		10
+# define MAX_EP		10
+# define MIN_AD		0
+
 # define APP_NAME	"ClapTrap "
 # define BLT_OK		" has been built correctly ..."
 # define DBLT_OK	" has been correctly deconstructed ..."
 # define ATTACK		" attacks "
 # define CAU		", causing "
 # define PT_DMG		" points of damage!"
-
+# define SF_RPR		" has sel-repaired in the amount of "
+# define POINTS		" points!"
+# define PT_MAX		" has gained more than 10 HP. Reset to max. 10 hit points!"
+# define RCV_DMG	" has received "
+# define RCV_DMG1	" point of damage!"
+# define RCV_DMG2	" points of damage!"
+# define DEATH		" has been destroyed!"
+# define DEATH1		" is destroyed!"
+# define WT_EP		" has 0 energy points! Impossible repair!!"
 
 # endif
--- Module_03/ex00/src/main.cpp
+++ Module_03/ex00/src/main.cpp
@@ -13,12 +13,12 @@
 #include "../includes/ClapTrap.hpp"
 
 /**
- * The function `main` 
- * 
- * 
- * 
- * 
- * 
+ * The `main` function serves as the entry point of the program. It 
+ * demonstrates the functionality of the `ClapTrap` class by creating an 
+ * instance and performing various actions such as attacking, taking damage, 
+ * and repairing.
+ *
+ * @return int  Returns `EXIT_SUCCESS` upon successful execution.
  * 
  */
 
@@ -26,10 +26,20 @@ int	main(void)
 {
 	print_banner();
 
-	
+	ClapTrap wall_e("Wall-e");
+	wall_e.attack("a box");
+	wall_e.takeDamage(6);
+	wall_e.attack("a 2nd box");
+	wall_e.beRepaired(15);
+	wall_e.attack("a 3rd box");
+	wall_e.attack("a 4th box");
+	wall_e.attack("a 5th box");
+	wall_e.attack("a 6th box");
+	wall_e.takeDamage(11);
 
+	wall_e.attack("a 7th box");
 
-
+	std::cout << std::endl;
 	
 	return (EXIT_SUCCESS);	
 }
