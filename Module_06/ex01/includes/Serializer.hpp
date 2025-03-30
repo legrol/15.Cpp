6c5acf7ecb92ee6b2beb466296c41847c6d9e823
rdel-olm <rdel-olm@student.42malaga.com>
2025-03-30 21:35:17 +0200

    Update Mod6 ex01 in progress

---
+++ Module_06/ex01/includes/Serializer.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdel-olm <rdel-olm@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/30 21:32:07 by rdel-olm          #+#    #+#             */
+/*   Updated: 2025/03/30 21:34:11 by rdel-olm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+// ============================================================================
+// Libraries
+// ============================================================================
+# include <iostream>			// std::cout, std::cerr, std::endl, ...
+# include <string>				// std::string
+# include <cstdlib>				// use to EXIT_FAILURE, EXIT_SUCCESS ...
+// # include <limits>				// fort numeric_limits
+// # include <cctype>				// for isdigit ...
+// # include <iomanip>				// std::setprecision
+// # include <cerrno>				// for errno and ERANGE...
+
+// ============================================================================
+// Access to my libraries
+// ============================================================================
+#include "macros.hpp"
+#include "colors.hpp"
+
+class Serializer
+{
+	private:
+
+
+
+
+
+	public:
+
+
+
+};
+
+// ============================================================================
+// Functions
+// ============================================================================
+void			print_banner();
