ad646a9406ea401b868db48be162a45f7c7fb52e
rdel-olm <rdel-olm@student.42malaga.com>
2025-03-31 12:22:44 +0200

    Update Mod06 ex02 in progress

---
+++ Module_06/ex02/src/main.cpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdel-olm <rdel-olm@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/01 00:52:31 by rdel-olm          #+#    #+#             */
+/*   Updated: 2025/03/30 21:53:36 by rdel-olm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Base.hpp"
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+Base* generate(void)
+{
+
+
+}
+
+void identify(Base* p)
+{
+
+
+}
+
+void identify(Base& p)
+{
+
+
+}
+
+int main(void)
+{
+	print_banner();
+
+	
+	
+
+
+
+	return (EXIT_SUCCESS);
+}
